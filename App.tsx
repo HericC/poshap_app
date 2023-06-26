f4f10c738b0765d90d4d45d97bd40918d2ed3257
Heric Macedo Oliveira Silva <heric_dev@hotmail.com>
2023-06-26 16:46:43 -0300

    feat: ignore all logs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { LoadingProvider } from './src/contexts/LoadingContext';
 import { AuthProvider } from './src/contexts/AuthContext';
 import Routes from './src/routes';
+
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
